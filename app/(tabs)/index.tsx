ee1b0fcd7e1c43e7b87ccddc7f795f41c0a9006a
bonjav <ajd.ankur9@gmail.com>
2025-07-12 16:42:02 +0800

    introduced push notifications

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,20 @@
+import { useNotification } from '@/contexts/NotificationContext';
 import NewsScreen from '@/screens/NewsScreen';
 
 export default function HomeScreen() {
+  const { notification, expoPushToken, error } = useNotification();
+
+  if(error){
+    console.error('Error registering for push notifications:', error);
+  }
+
+  if(expoPushToken){
+    console.log('Push token:', expoPushToken);
+  }
+
+  if(notification){
+    console.log('Notification:', notification);
+  }
+
   return <NewsScreen />;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,21 @@ import { SplashScreen } from 'expo-router';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { SettingsProvider } from '@/contexts/SettingsContext';
 import { NewsProvider } from '@/contexts/NewsContext';
+import * as Notifications from 'expo-notifications';
 //import '@/services/syncService'; // Initialize sync service
+import { registerForPushNotificationsAsync } from '@/util/registerForPushNotificationsAsync';
+import { NotificationProvider } from '@/contexts/NotificationContext';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync().catch(() => {
@@ -38,16 +52,18 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider>
-      <SettingsProvider>
-        <NewsProvider>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
-          </Stack>
-          <StatusBar style="auto" />
-        </NewsProvider>
-      </SettingsProvider>
-    </ThemeProvider>
+    <NotificationProvider>
+      <ThemeProvider>
+        <SettingsProvider>
+          <NewsProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="+not-found" options={{ title: 'Not Found' }} />
+            </Stack>
+            <StatusBar style="auto" />
+          </NewsProvider>
+        </SettingsProvider>
+      </ThemeProvider>
+    </NotificationProvider>
   );
 }
